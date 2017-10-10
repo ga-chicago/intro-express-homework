1205ae6271335f7904db7c4cd9593cb44855230b
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-10-10 17:06:20 -0500

    Commit 7 - show view shows req.params.id

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.get("/pokemon", (req, res) => {
 res.render('index',{pokemon});
 })
 
+app.get("/pokemon/:id", (req, res) => {
+res.render('show',{poke: pokemon[req.params.id]});
+})
+
+
 app.listen(3000, () => {
 	console.log("listening on port 3000");
 })
