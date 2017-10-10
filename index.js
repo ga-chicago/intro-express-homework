79d39c44ffb351c609e6d04c51bf6e00b0da34e9
Collin Cromwell <collcrom@gmail.com>
2017-10-10 17:38:43 -0500

    index.ejs rendered at pokemon route

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const pokemon = require('./models/pokemon');
 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
 app.get('/', (req, res) => {
 	res.send(`Welcome to the Poke'Center`)
 })
 
 app.get('/pokemon', (req, res) => {
-	res.send(pokemon);
+	res.render('index', {pokemon});
 })
 
 
