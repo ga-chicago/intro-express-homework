d226001a4e59dcaec218f63feeffb4b939e917e4
Collin Cromwell <collcrom@gmail.com>
2017-10-10 17:31:52 -0500

    connected my database, can see json in the browser

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const pokemon = require('./models/pokemon');
 
 app.get('/', (req, res) => {
 	res.send(`Welcome to the Poke'Center`)
 })
 
+app.get('/pokemon', (req, res) => {
+	res.send(pokemon);
+})
 
 
 app.listen(3000, () =>{
