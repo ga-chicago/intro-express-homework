02637c933e17d025e781507b60ec5d35af020f2e
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-10-10 16:56:47 -0500

    Commit 5 - index.ejs view rendered at pokemon route

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const app = express();
 const pokemon = require('./models/pokemon')
 
+//tell express where view is
+app.set('views',__dirname + '/views');
+app.use('/static', express.static('public'));
+app.set('view engine', 'ejs');
+
+//
+
 app.get("/", (req, res) => {
-res.send("welcome to the pokemon app");
+	res.send("welcome to the pokemon app");
 })
 
 
 app.get("/pokemon", (req, res) => {
-res.send(pokemon);
+res.render('index',{pokemon});
 })
-
 
 app.listen(3000, () => {
 	console.log("listening on port 3000");
