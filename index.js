7c040efa573dd40d0c236a1979c78a6cbebe4088
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-10-10 16:31:25 -0500

    Commit 4 - Connected my database, can see json in the browser

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const pokemon = require('./models/pokemon')
 
 app.get("/", (req, res) => {
 res.send("welcome to the pokemon app");
 })
+
+
+app.get("/pokemon", (req, res) => {
+res.send(pokemon);
+})
+
 
 app.listen(3000, () => {
 	console.log("listening on port 3000");
