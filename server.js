b40cfcb4ceb0b5585a84eef0ccacc99427870ce7
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-10-10 16:37:07 -0500

    Commit 3 - My server is set up and running

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+	res.send('Welcome to the Pokemon App!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+	console.log('port 3000 ready for action');
+});
