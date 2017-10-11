a4da14743f7b386898cbfe374cec27a7d3fb2d47
sjgraffjr <sjgraffjr@gmail.com>
2017-10-10 21:46:08 -0500

    Commit 9 - created show views of each pokemon

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
 app.get('/pokemon',(req,res)=>{
-	res.render('index', {
-						pokemonIndex: pokemon
-						});  
+	res.render('index', {pokemon});  
 })
 
 app.get('/pokemon/:id',(req,res)=>{
-	res.send(req.params.id);
+	res.render('show',pokemon[req.params.id]);
 })
 
 app.listen(3000, ()=>{
