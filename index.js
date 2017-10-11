0b0d6628adfbb3de648428fa7f4aca62309da7a7
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-10-10 19:31:08 -0500

    Commit 3 - My server is set up and running

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+	res.send('Welcome to the Pokemon App!');
+});
+
+
+
+
+
+app.listen(3000, () => {
+	console.log('This is the Poke Express on port 3000')
+})
