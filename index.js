c5d3898e81a93b2d47504eeca21ace438cb9e621
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-10-10 19:44:41 -0500

    Commit 5 - index.ejs view rendered at pokemon route

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const pokemon = require('./models/pokemon');
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+
+
 
 app.get('/', (req, res) => {
 	res.send('Welcome to the Pokemon App!');
 });
 
 app.get('/pokemon', (req, res) => {
-	res.send(pokemon);
+	res.render('index');
 });
 
 
