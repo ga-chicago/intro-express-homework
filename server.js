8616f2415b77b68b857bdf7a4f2011a13bbf230f
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-10-10 19:33:33 -0500

    created show views of each pokemon

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/pokemon', (req, res) => {
 })
 
 app.get('/pokemon/:id', (req, res) => {
-	res.send(req.params.id)
+	res.render('show', {poke: pokemon[req.params.id]})
 })
 
 app.listen(3000, () => {
