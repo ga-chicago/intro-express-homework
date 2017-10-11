70a5aec0ad6c4b251bd6de09105a9b4326b64beb
Sam Sheehan <ssheehan1210@yahoo.com>
2017-10-11 05:17:57 -0500

    Commit 5 - index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const pokemon = require('./models/pokemon');
 
 
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+
 app.get('/', (req, res) => {
 	res.send('Welcome to the Pokemon App!');
 })
 
 app.get('/pokemon', (req, res) => {
-	res.send(pokemon);
+	res.render('index', {
+		allPokemon: pokemon
+	});
 })
 
 
