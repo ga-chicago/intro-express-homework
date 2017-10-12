e3633db52c692ef9672f69ca3084b0f52e942725
sjgraffjr <sjgraffjr@gmail.com>
2017-10-12 18:20:21 -0500

    got delete and edit page working

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -14,8 +14,32 @@ router.get('/:id',(req,res)=>{
 	res.render('show',pokemon[req.params.id]);
 })
 
+router.get('/:index/edit',(req,res)=>{
+	res.render('edit', {
+		pokemon: pokemon[req.params.index],
+		index: req.params.index
+	})
+})
+
 router.post('/create',(req,res)=>{
 	console.log(req.body)
+	pokemon.push(req.body)
+	res.redirect('/pokemon')
+})
+router.put('/:index/edit',(req,res)=>{
+	console.log(req.body)
+	
+
+	pokemon[req.params.index] = req.body
+	res.redirect('/pokemon')
+})
+
+router.delete('/:index', (req,res)=>{ //listening for delete request
+	console.log('hitting the delete route')
+	console.log(req.params.index)
+	//model is removing the item out of the array in models folder
+	pokemon.splice(req.params.index, 1);
+	res.redirect('/pokemon')
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 //add create route
 //add new route
 //add a controller
@@ -10,7 +11,7 @@ app.set('views', __dirname + '/views')
 //
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'))
-
+app.use(methodOverride('_method'));
 
 app.use('/pokemon', pokemonController)
 
