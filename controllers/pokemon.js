7e07ccae619a792cfa3aeda72818a63f7873de9c
Collin Cromwell <collcrom@gmail.com>
2017-10-12 18:23:16 -0500

    added put and delete

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -14,14 +14,23 @@ router.get('/pokemon/new', (req, res) =>{
 	res.render('new', {});
 })
 
+router.put('/pokemon/:index', (req, res) =>{
+	pokemon[req.params.index] = req.body
+	res.redirect('/pokemon');
+})
+
+router.delete('/pokemon/:index', (req, res) =>{
+	pokemon.splice([req.params.index], 1);
+	res.redirect('/pokemon');
+})
+
 router.post('/pokemon/create', (req, res)=>{
-	console.log(req.body)
 	pokemon.push(req.body);
 	res.redirect('/pokemon');
 })
 
 router.get('/pokemon/:id', (req, res) => {
-	res.render('shows', pokemon[req.params.id]);
+	res.render('shows', {pokemon: pokemon[req.params.id], i: req.params.id});
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const pokemonController = require('./controllers/pokemon')
 
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
 
