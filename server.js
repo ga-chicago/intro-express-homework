51b43a5c7d75ba3741a7b28fdca9372855adbdf5
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-10-12 16:55:08 -0500

    update and delte now functional

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 
 const pokemonController = require('./controllers/pokemon')
 
@@ -10,6 +11,8 @@ app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(methodOverride('_method'))
 
 app.use('/', pokemonController)
 
