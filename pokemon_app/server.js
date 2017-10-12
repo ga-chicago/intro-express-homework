82314493e1e316f4dc46e9cd220531ad49fa9421
Myka Martinez <michaelmartinez-mann@Yin.local>
2017-10-11 23:22:19 -0500

    Commit 4 - Connected my database, cannot see json in the browser, only object array

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = 3000
 
+
+const pokemon = require("./models/pokemon.js")
 app.listen(port, () => {
 	console.log("Gotta catch 'em all!")
 }) 
@@ -11,3 +13,6 @@ app.get("/", (req, res) => {
 	res.send("Welcome to the Pokemon App!")
 })
 
+app.get("/pokemon", (req, res) => {
+		res.send(JSON.stringify(pokemon))
+})
