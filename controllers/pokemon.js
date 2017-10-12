f055e0619f73a8c4db47a28612e078f744c524aa
Todd herskovitz <toddherskovitz@Todds-MacBook-Pro.local>
2017-10-12 17:43:09 -0500

    added delete and put

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -15,6 +15,26 @@ router.get("/", (req, res) => {
 	res.render('index',{pokemon});
 })
 
+router.delete('/:index', (req, res) => {
+  console.log(req.params.index)
+
+  pokemon.splice(req.params.index, 1);
+  res.redirect('/pokemon')
+})
+
+router.get('/:index/edit', (req, res) => {
+  res.render('edit', {
+                      pokemon: pokemon[req.params.index],
+                      index: req.params.index
+                      })
+});
+
+router.put('/:index/edit', (req, res) => {
+  pokemon[req.params.index] = req.body;
+
+  res.redirect('/pokemon')
+})
+
 router.get("/:id", (req, res) => {
 	res.render('show',{poke: pokemon[req.params.id]});
 })
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 //require controllers
 const pokemonController = require('./controllers/pokemon');
 //tell express where view is
 //set up your ejs and folders
 app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views');
-app.use('/static', express.static('public'));
 
+//use
+app.use('/static', express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'));
 
 //put the controller at the end of middleware
 app.use('/pokemon', pokemonController)
