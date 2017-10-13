26120b3abcf49483dbed110c07e15717a2c34f22
Sam Sheehan <ssheehan1210@yahoo.com>
2017-10-13 10:41:18 -0500

    Created delete and edit methods and added new pages and buttons to allow users to run such methods

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -22,15 +22,45 @@ router.get('/:index', (req, res) => {
 
 
 
+router.get('/:index/edit', (req, res) => {
+	res.render('edit', {
+		allPokemon: pokemon[req.params.index],
+		index: req.params.index
+	});
+})
+
+
+
 router.post('/create', (req, res) => {
 	console.log(req.body);
-
 	if (req.body.placement === 'on') {
-		req.body.placement = req.body.length;
+		req.body.placement = true;
+	} else {
+		req.body.placement = false;
 	}
-
 	pokemon.push(req.body);
-	
+	res.redirect('/pokemon');
+})
+
+
+
+router.put('/:index/edit', (req, res) => {
+	console.log(req.body);
+	if (req.body.placement === 'on') {
+		req.body.placement = true;
+	} else {
+		req.body.placement = false;
+	}
+	pokemon[req.params.index] = req.body;
+	res.redirect('/pokemon');
+})
+
+
+
+router.delete('/:index', (req, res) => {
+	console.log('hitting the delete route');
+	console.log(req.params.index);
+	pokemon.splice(req.params.index, 1);
 	res.redirect('/pokemon');
 })
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 
 
@@ -18,6 +19,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use(express.static('public'));
+
+
+
+app.use(methodOverride('_method'));
 
 
 
